7cc3608a1adb9368ac791ba0c4d3daea8b1e6b37
deepak <deepak.sahootesting@gmail.com>
2020-01-13 13:39:26 +0530

    testingcode

---
+++ src/test/java/com/twitter/statuses/UserTimeLineTest.java
@@ -0,0 +1,54 @@
+package com.twitter.statuses;
+
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import twitter.common.RestUtility;
+import twitter.constances.EndPoints;
+import twitter.constances.Path;
+
+import java.util.ArrayList;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.hasItem;
+
+public class UserTimeLineTest {
+
+    RequestSpecification requestSpecification;
+    ResponseSpecification responseSpec;
+
+    @BeforeClass
+    public void setUp() {
+        requestSpecification = RestUtility.getRequestSpecification();
+        requestSpecification.queryParam("user_id", "deepak");
+        requestSpecification.basePath(Path.STATUSES);
+        responseSpec = RestUtility.getResponseSpecification();
+    }
+
+    @Test
+    public void readTweetsOne() {
+        given()
+                //.spec(requestSpecification)
+                .spec(RestUtility.createQueryParam(requestSpecification, "count", "1"))
+                .when()
+                .get(EndPoints.STATUSES_USER_TIMELINE)
+                .then()
+                .log()
+                .all()
+                .spec(responseSpec)
+                .body("user.screen_name", hasItem("deepak"));
+    }
+
+    @Test
+    public void readTweetsTwo() {
+        RestUtility.setEndPoint(EndPoints.STATUSES_USER_TIMELINE);
+
+        Response response = RestUtility.getResponse(RestUtility.createQueryParam(requestSpecification, "count", "1"), "get");
+        ArrayList<String> screenName = response.path("user.screen_name");
+        System.out.println(screenName);
+        Assert.assertTrue(screenName.contains("deepak"));
+    }
+}
